b7f9c5148c04622afcbc8e92229ca2ff5b4caa13
qurdafia <fragrancegarden@gmail.com>
2016-11-04 17:27:28 +0800

    Updating the files with

--- blog/views.py
+++ blog/views.py
@@ -3,12 +3,15 @@ from django.http import HttpResponse, HttpResponseRedirect
 from .models import Post
 from .forms import PostForm
 from django.contrib import messages
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+
 
 # Create your views here.
 
 
 def post_create(request):
-    form = PostForm(request.POST or None)
+    form = PostForm(request.POST or None, request.FILES or None)
     if form.is_valid():
         instance = form.save(commit=False)
         instance.save()
@@ -21,8 +24,8 @@ def post_create(request):
     }
     return render(request, 'post_form.html', context)
 
-def post_detail(request, id):
-    instance = get_object_or_404(Post, id=id)
+def post_detail(request, slug):
+    instance = get_object_or_404(Post, slug=slug)
     context = {
         'title': 'Detail',
         'instance': instance,
@@ -30,17 +33,32 @@ def post_detail(request, id):
     return render(request, 'post_detail.html', context)
 
 def post_list(request):
-    queryset = Post.objects.all().order_by('-timestamp')
+    queryset_list = Post.objects.all().order_by('-timestamp')
+    paginator = Paginator(queryset_list, 6)  # Show 25 contacts per page
+    page_req_var = 'list_page'
+
+    page = request.GET.get(page_req_var)
+
+    try:
+        queryset = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        queryset = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        queryset = paginator.page(paginator.num_pages)
 
     context = {
         'object_list': queryset,
-        'title': 'List'
+        'title': 'List',
+        'page_req_var': page_req_var,
+
     }
     return render(request, 'post_list.html', context)
 
-def post_update(request, id=None):
-    instance = get_object_or_404(Post, id=id)
-    form = PostForm(request.POST or None, instance=instance)
+def post_update(request, slug=None):
+    instance = get_object_or_404(Post, slug=slug)
+    form = PostForm(request.POST or None, request.FILES or None, instance=instance)
     if form.is_valid():
         instance = form.save(commit=False)
         instance.save()
@@ -55,8 +73,8 @@ def post_update(request, id=None):
     }
     return render(request, 'post_form.html', context)
 
-def post_delete(request, id=None):
-    instance = get_object_or_404(Post, id=id)
+def post_delete(request, slug=None):
+    instance = get_object_or_404(Post, slug=slug)
     instance.delete()
     # message success
     messages.success(request, "Successfully Deleted")
--- fc_blog/urls.py
+++ fc_blog/urls.py
@@ -15,10 +15,16 @@ Including another URLconf
     3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
 from django.conf.urls import include, url
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^', include('blog.urls')),
+    #url(r'^', include('blog.urls')),
     url(r'^admin/', admin.site.urls),
-    url(r'^blog/', include('blog.urls', namespace='blog')),
+    url(r'^', include('blog.urls', namespace='blog')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
